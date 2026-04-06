28153e9dd661a3b94f80f02de275352728f1ccec
Kowalski1337 <vadem1998@gmail.com>
2026-04-06 10:39:47 +0200

    reactive atom

--- src/habits/frontend/core.cljs
+++ src/habits/frontend/core.cljs
@@ -1,7 +1,8 @@
 (ns habits.frontend.core
-  (:require [reagent.dom :as rdom]))
+  (:require [reagent.dom :as rdom]
+            [reagent.core :as r]))
 
-(defonce state (atom {:message "Loading..."}))
+(defonce state (r/atom {:message "Loading..."}))
 
 (defn fetch-test []
       (-> (js/fetch "http://localhost:3000/api/test")
@@ -12,15 +13,15 @@
                       (swap! state assoc :message (str "Error: " err))))))
 
 (defn app []
-      [:div.container.mx-auto.p-8
-       [:h1.text-3xl.font-bold.text-blue-600 "Habit Tracker"]
-       [:p.mt-4 "Message from server: " @state]
-       [:button.bg-green-500.text-white.px-4.py-2.rounded.mt-4
-        {:on-click fetch-test}
-        "Fetch from API"]])
+  (let [state @state]  ;; ← читаем атом один раз в начале
+    [:div.container.mx-auto.p-8
+     [:h1.text-3xl.font-bold.text-blue-600 "Habit Tracker"]
+     [:p.mt-4 "Message from server: " (:message state)]
+     [:button.bg-green-500.text-white.px-4.py-2.rounded.mt-4
+      {:on-click fetch-test}
+      "Fetch from API"]]))
 
 (defn init []
-      (fetch-test)
       (rdom/render [app] (js/document.getElementById "app")))
 
 (defn ^:dev/after-load reload []
